cf8d718f6b0bb51fae57486e61ad7eb1e3fc7443
João Costa <101182024+joaolscosta@users.noreply.github.com>
2022-04-08 21:24:57 +0100

    lab10 update iaed
    exercise 1

---
+++ iaed/labs/IAED_Lab10/teste.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Date
+{
+    int day;
+    int month;
+    int year;
+} Date;
+
+int main()
+{
+    int n, i;
+    Date *dates, *p;
+
+    scanf("%d", &n);
+
+    dates = malloc((sizeof(Date)) * n);
+
+    for(i = 0; i < n - 1; i++)
+    {
+        p = dates + i;
+        scanf("%02d-%02d-%04d\n", &p->day, &p->month, &p->year);
+    }
+
+    for(i = n - 1; i >= 0; i--)
+    {
+        p = dates + i;
+        printf("%02d-%02d-%04d\n", p->day, p->month, p->year);
+    }
+
+    return 0;
+}
